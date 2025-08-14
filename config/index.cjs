419bfa57c64d9ed6857aea2419e3be0238456345
soul-horizon <147786410+soul-horizon@users.noreply.github.com>
2025-08-14 13:09:26 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,9 +21,9 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '网',
+      name: '小怡',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'olcOvvpyBhHLH0WuTwFqTF11UpPA',
+      id: 'olcOvvvJJaEWVDRvcE6O3zN4snGE',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'lFQ76D1RYaVaT2fkr_fngF1PJY6Istl47Ms_N8rETAk',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
