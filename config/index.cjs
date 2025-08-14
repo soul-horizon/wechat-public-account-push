3bdb6d69c854881b711488adb2ac1ecf068e2e95
soul-horizon <147786410+soul-horizon@users.noreply.github.com>
2025-08-14 12:00:59 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -25,17 +25,17 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'olcOvvpyBhHLH0WuTwFqTF11UpPA',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'JxwsHtVUwqxAyX9x2MlFXClZ5jEPEDGsold8-fERR9c',
+      useTemplateId: ' 	la4x5j7lWFie_4cAk9EGUNbow6xf7O95ZYz2yixb-en',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '04-03',
+      horoscopeDate: '05-06',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '2003', date: '04-03',
+          type: '*生日', name: '宝贝', year: '2003', date: '05-06',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '2003', date: '04-03',
+          type: '生日', name: '李四', year: '2003', date: '05-06',
         },
         {
           type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
