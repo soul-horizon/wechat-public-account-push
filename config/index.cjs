8fb0624d14a0b0dea0d697a75d14652b0fa87d35
soul-horizon <147786410+soul-horizon@users.noreply.github.com>
2025-08-14 09:27:18 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -59,7 +59,7 @@ const USER_CONFIG = {
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'JxwsHtVUwqxAyX9x2MlFXClZ5jEPEDGsold8-fERR9c',
+      id: 'olcOvvpyBhHLH0WuTwFqTF11UpPA',
     }
   ],
 
