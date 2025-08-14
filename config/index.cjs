3c9765bc4e00cc7df12ddebe176e5bb5447f9388
soul-horizon <147786410+soul-horizon@users.noreply.github.com>
2025-08-14 12:30:43 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -25,7 +25,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'olcOvvpyBhHLH0WuTwFqTF11UpPA',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: ' 	lFQ76D1RYaVaT2fkr_fngF1PJY6Istl47Ms_N8rETAk',
+      useTemplateId: 'lFQ76D1RYaVaT2fkr_fngF1PJY6Istl47Ms_N8rETAk',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '05-06',
       festivals: [
